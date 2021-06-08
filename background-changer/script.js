65ac892b448c69740453968de2b8357a523148af
paola-machado <paolamds06@gmail.com>
2021-06-08 20:37:56 -0300

    feat: add background changer

---
+++ background-changer/script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  document.body.style.background = randomBg();
+});
+
+function randomBg() {
+  return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+}
